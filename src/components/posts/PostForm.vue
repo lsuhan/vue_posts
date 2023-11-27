1f8b1a63c92ea26c2a11388118cad2b5358879e8
lsuhan00 <lsuhan00@hanmail.net>
2023-11-27 17:22:29 +0900

    글로벌 컴포넌트 등록, 커스텀 디렉티브 등록, dayjs 플러그인 등록

--- src/components/posts/PostForm.vue
+++ src/components/posts/PostForm.vue
@@ -2,7 +2,10 @@
 	<form>
 		<div class="mb-3">
 			<label for="exampleFormControlInput1" class="form-label">제목</label>
+			<!--autofocus 는 적용안됨 왜냐면 동적이라 그래 autifocus는 한번만 실행됨-->
 			<input
+				v-focus
+				v-color="'blue'"
 				:value="title"
 				@input="$emit('update:title', $event.target.value)"
 				type="text"
@@ -27,6 +30,11 @@
 </template>
 
 <script setup>
+// const vFocus = {
+// 	mounted: el => {
+// 		el.focus();
+// 	},
+// };
 defineProps({
 	title: String,
 	content: String,
--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import person from './plugins/person';
 import { createApp } from 'vue';
 import App from './App.vue';
 import globalComponents from './plugins/global-components';
+import globalDirectives from './plugins/global-directives';
+import dayjs from './plugins/dayjs';
 
 const app = createApp(App);
+// app.directive('focus', focus); //커스텀 디렉티브
+app.use(globalDirectives); //global 파일 하나 만들어서 install 시킨 컴포넌트
 app.use(globalComponents); //주석해도 잘나옴왜 ??? 플러그인이 자동으로 import시켜줌
+app.use(dayjs);
 app.use(funcPlugins); //플러그인 등록
 app.use(objPlugins, { name: '짐코딩' }); // 플러그인 등록
 app.use(person, { name: '홍길동' });
